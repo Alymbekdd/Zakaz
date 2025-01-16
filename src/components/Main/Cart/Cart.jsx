6fd31b759a42ba789dafda2c04f275d428a80424
Alymbekdd <usekowalymbek2002@gmail.com>
2025-01-17 00:25:24 +0600

    1
    1

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import item from '../../img/H20c9d9bb99974e729c20451549fd58cfD.jpg_640x640.avif';
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
@@ -9,19 +10,62 @@ const Cart = () => {
         <p className='Cart__count'>Количество товаров 27</p>
 
         <section className='Menu'>
+
           <div className="Menu__items">
             <img src={item} alt="" />
             <div className="Menu__content">
-              
+
               <div className="Menu__content-info">
-              <h3>Женские нижние белья</h3>
-              <h3>670 сом</h3>
-              <p>отличное качество</p>
-              <p>новинка!</p>
+                <h3>Женские нижние белья</h3>
+                <h3>670 сом</h3>
+                <p>отличное качество</p>
+                <p>новинка!</p>
               </div>
 
               <div className="Menu__content-btns">
-                <button className='Menu__content-btn'>Заказать</button>
+                <Link className='Menu__content-link' to={'/Buy'}>
+                  <button className='Menu__content-btn'>Заказать</button>
+                </Link>
+                <button className='Menu__content-delite'>Удалить</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="Menu__items">
+            <img src={item} alt="" />
+            <div className="Menu__content">
+
+              <div className="Menu__content-info">
+                <h3>Женские нижние белья</h3>
+                <h3>670 сом</h3>
+                <p>отличное качество</p>
+                <p>новинка!</p>
+              </div>
+
+              <div className="Menu__content-btns">
+                <Link className='Menu__content-link' to={'/Buy'}>
+                  <button className='Menu__content-btn'>Заказать</button>
+                </Link>
+                <button className='Menu__content-delite'>Удалить</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="Menu__items">
+            <img src={item} alt="" />
+            <div className="Menu__content">
+
+              <div className="Menu__content-info">
+                <h3>Женские нижние белья</h3>
+                <h3>670 сом</h3>
+                <p>отличное качество</p>
+                <p>новинка!</p>
+              </div>
+
+              <div className="Menu__content-btns">
+                <Link className='Menu__content-link' to={'/Buy'}>
+                  <button className='Menu__content-btn'>Заказать</button>
+                </Link>
                 <button className='Menu__content-delite'>Удалить</button>
               </div>
             </div>
